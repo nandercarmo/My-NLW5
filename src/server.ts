35219ea26c699e085909c324e4859e8810a27474
NanderSantos <nandermgsdp@gmail.com>
2021-04-19 10:46:31 -0300

    Adiçao do dia 01. Configuraçao do projeto com o TypeScript. Criaçao do servidor e testes das primeiras rotas HTTP GET e POST.

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+	return res.json({
+		message: "Olá NLW 05!"
+	});
+});
+
+app.post("/", (req, res) => {
+	return res.json({
+		message: "User saved!"
+	});
+});
+
+app.listen(3333, () => console.log("Server is running on port: 3333"));
